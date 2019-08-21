94496077e7a3e8a643b72e2840f72142a67d5e24
barton-w <bartonw@gmail.com>
2019-08-20 19:01:06 -0600

    Updates

---
+++ precache-manifest.9f7c8fba45f4e84f5b0397b4c149ebec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ea5c0060e78cc0999859abd1c8f5ff",
+    "url": "/song-explorer/index.html"
+  },
+  {
+    "revision": "524eea1f1ef73c47cb10",
+    "url": "/song-explorer/static/css/main.cd91090c.chunk.css"
+  },
+  {
+    "revision": "17ec2908c9f3b4a6b6c8",
+    "url": "/song-explorer/static/js/2.28cdb552.chunk.js"
+  },
+  {
+    "revision": "524eea1f1ef73c47cb10",
+    "url": "/song-explorer/static/js/main.e41b0785.chunk.js"
+  },
+  {
+    "revision": "a1c16af78ff1348b3014",
+    "url": "/song-explorer/static/js/runtime~main.66190786.js"
+  },
+  {
+    "revision": "73f56e293774c5c88177356c038a4be1",
+    "url": "/song-explorer/static/media/genius-logo.73f56e29.png"
+  }
+]);
